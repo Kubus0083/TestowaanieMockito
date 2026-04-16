ee67d2a88da268ef8edd0ce29d10061db6e05a4a
Jakub Szczepaniak <128096249+Kubus0083@users.noreply.github.com>
2026-04-17 00:35:04 +0200

    Add files via upload

--- ZadanieTest/src/main/java/org/example/Ksiazka.java
+++ ZadanieTest/src/main/java/org/example/Ksiazka.java
@@ -13,13 +13,11 @@ public class Ksiazka {
         this.rokWydania = rokWydania;
     }
 
-    // Gettery
     public int getId() { return id; }
     public String getTytul() { return tytul; }
     public String getAutor() { return autor; }
     public int getRokWydania() { return rokWydania; }
 
-    // Settery (potrzebne przy aktualizacji)
     public void setTytul(String tytul) { this.tytul = tytul; }
     public void setAutor(String autor) { this.autor = autor; }
     public void setRokWydania(int rokWydania) { this.rokWydania = rokWydania; }
